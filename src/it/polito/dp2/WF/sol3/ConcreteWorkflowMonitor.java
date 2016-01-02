826126cbf387beedf22b9d77328330ce829de214
Luke <luca.mannella@outlook.it>
2016-01-02 22:59:28 +0100

    The class ConcreteWorkflowMonitor was implemented.

--- src/it/polito/dp2/WF/sol3/ConcreteWorkflowMonitor.java
+++ src/it/polito/dp2/WF/sol3/ConcreteWorkflowMonitor.java
@@ -1,17 +1,71 @@
 package it.polito.dp2.WF.sol3;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeSet;
+
+import javax.xml.datatype.XMLGregorianCalendar;
+import javax.xml.ws.Holder;
+
+import com.oracle.xmlns.internal.webservices.jaxws_databinding.ObjectFactory;
 
 import it.polito.dp2.WF.ProcessReader;
 import it.polito.dp2.WF.WorkflowMonitor;
+import it.polito.dp2.WF.WorkflowMonitorException;
 import it.polito.dp2.WF.WorkflowReader;
 import it.polito.dp2.WF.lab3.Refreshable;
+import it.polito.dp2.WF.lab3.gen.UnknownNames_Exception;
+import it.polito.dp2.WF.lab3.gen.Workflow;
+import it.polito.dp2.WF.lab3.gen.WorkflowInfo;
+import it.polito.dp2.WF.lab3.gen.WorkflowInfoService;
 
+/**
+ * This is a concrete implementation of the interface WorkflowMonitor based on the JAXB framework.<BR><BR>
+ * If you want more detail about the interface look to {@link it.polito.dp2.WF.WorkflowMonitor}
+ * 
+ * @author Luca
+ */
 public class ConcreteWorkflowMonitor implements WorkflowMonitor, Refreshable {
 	
-	public ConcreteWorkflowMonitor() {
-		// TODO Auto-generated constructor stub
-		// for taking the URL use this system property = "it.polito.dp2.WF.sol3.URL"
+	private Map<String, WorkflowReader> workflows;
+	private Set<ProcessReader> processes;
+	
+	public ConcreteWorkflowMonitor() throws MalformedURLException, WorkflowMonitorException {		// TODO ConcreteWorkflowMonitor Constructor
+		workflows = new HashMap<String, WorkflowReader>();
+		processes = new HashSet<ProcessReader>();		// it must remains empty
+		
+		// taking the URL of the Web Service
+		String webServiceString = System.getProperty("it.polito.dp2.WF.sol3.URL");
+		URL webServiceURL = new URL(webServiceString);
+		
+		// taking the port (proxy) from the service
+		WorkflowInfoService service = new WorkflowInfoService(webServiceURL);
+		WorkflowInfo proxy = service.getWorkflowInfoPort();
+		
+		System.out.println("Retrieving the names of the workflows...");
+		Holder<XMLGregorianCalendar> calendarHolder = new Holder<XMLGregorianCalendar>();
+		Holder<List<String>> workflowNamesHolder = new Holder<List<String>>();
+		proxy.getWorkflowNames(calendarHolder, workflowNamesHolder);
+		
+		Holder<List<Workflow>> workflowsHolder = new Holder<List<Workflow>>();
+		try {
+			System.out.println("Retrieving the workflows...");
+			proxy.getWorkflows(workflowNamesHolder.value, calendarHolder, workflowsHolder);
+		} catch (UnknownNames_Exception e) {
+			throw new WorkflowMonitorException("Error retrieving the workflows: "+e.getMessage());
+		}
+		
+		//build the WorkflowReaderSet
+		for( Workflow wf: workflowsHolder.value ) {
+			WorkflowReader wfr = new ConcreteWorkflowReader(wf);
+			workflows.put(wfr.getName(), wfr);
+		}
+		
 	}
 
 	@Override
@@ -27,20 +81,30 @@ public class ConcreteWorkflowMonitor implements WorkflowMonitor, Refreshable {
 
 	@Override
 	public Set<ProcessReader> getProcesses() {
-		// TODO Auto-generated method stub
-		return null;
+		return processes;
 	}
 
 	@Override
-	public WorkflowReader getWorkflow(String arg0) {
-		// TODO Auto-generated method stub
-		return null;
+	public WorkflowReader getWorkflow(String name) {
+		return workflows.get(name);
 	}
 
 	@Override
 	public Set<WorkflowReader> getWorkflows() {
-		// TODO Auto-generated method stub
-		return null;
+		return new TreeSet<WorkflowReader>(workflows.values());
+	}
+	
+	public String toString(){
+		StringBuffer buf = new StringBuffer("Inside this WorkflowMonitor there are:\n");
+		
+		if((workflows==null) || (workflows.isEmpty()))
+			buf.append("\tNo Workflows\n");
+		else {
+			for(WorkflowReader wfr : workflows.values())
+				buf.append("\t"+wfr.toString()+"\n");
+		}
+				
+		return buf.toString();
 	}
 
 }
--- src/it/polito/dp2/WF/sol3/WorkflowMonitorFactory.java
+++ src/it/polito/dp2/WF/sol3/WorkflowMonitorFactory.java
@@ -1,14 +1,43 @@
 package it.polito.dp2.WF.sol3;
 
+import java.net.MalformedURLException;
+
 import it.polito.dp2.WF.WorkflowMonitor;
 import it.polito.dp2.WF.WorkflowMonitorException;
 
 public class WorkflowMonitorFactory extends it.polito.dp2.WF.WorkflowMonitorFactory {
-
+	
+	/**
+	 * This method creates an instance of my concrete class that implements the WorkflowMonitor interface.
+	 */
 	@Override
 	public WorkflowMonitor newWorkflowMonitor() throws WorkflowMonitorException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		WorkflowMonitor myMonitor = null;
+		try {
+			myMonitor = new ConcreteWorkflowMonitor();
+		} catch (MalformedURLException e) {
+			System.err.println("Error parsing the URL: "+e.getMessage());
+			e.printStackTrace();
+			throw new WorkflowMonitorException("Error parsing the given URL: "+e.getMessage());
+		} catch (WorkflowMonitorException e) {
+			System.err.println("Error: "+e.getMessage());
+			e.printStackTrace();
+			throw e;
+		}
+		
+		/* catch (JAXBException | SAXException e) {
+			System.err.println("Error: "+e.getMessage());
+			e.printStackTrace();
+			throw new WorkflowMonitorException(e.getMessage());
+		}*/
+		
+		return myMonitor;
+	}
+	
+	@Override
+	public String toString(){
+		return "This is a custom WorkflowMonitorFactory implementation for the assignment 3.";
 	}
 
 }
